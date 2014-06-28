7bdca2b3cc2d72355ad83c10304c8b0c714e87a2
jeffsgraham <jsgraham@uw.edu>
2014-06-27 18:30:26 -0700

    Update private_settings.py

--- jarvis/jarvis/private_settings.py
+++ jarvis/jarvis/private_settings.py
@@ -1,3 +1,6 @@
+# Contains settings that should not be shared with others
+# Update all values to match each deployment.
+# The following are just example values
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'd5aks6$9ubf8=@bvxl_as=ij_jc79xwilk%8k4wtlpu_&ic5hr'
