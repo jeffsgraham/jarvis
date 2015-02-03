ace72b583a3d972760f9cc50d197b6738158f242
Jeff Graham <jsgraham@uw.edu>
2015-02-03 23:50:31 +0000

    added text_views.py to tracked files

---
+++ jarvis/inventory/tests/test_views.py
@@ -0,0 +1,120 @@
+from django.test import TestCase
+from django.contrib.auth.models import User
+from inventory.models import *
+
+class AbstractViewTests():
+    base_url = "/"
+    username = "test"
+    password = "password"
+    context_keys = []
+
+    def setUp(self):
+        User.objects.create_user(username=self.username, password=self.password)
+
+class AbstractViewGetTests(AbstractViewTests):
+    def test_login_required(self):
+        resp = self.client.get(self.base_url, follow=True)
+        self.assertEqual(len(resp.redirect_chain), 1) #exactly one redirect
+        #redirect is to login page
+        self.assertTrue(resp.redirect_chain[0][0].startswith("http://testserver/accounts/login/?next=" + self.base_url))
+        self.assertEqual(resp.redirect_chain[0][1], 302) #http code is 302
+
+    def test_resp_code(self):
+        self.client.login(username=self.username, password=self.password)
+        resp = self.client.get(self.base_url)
+        self.assertEqual(resp.status_code, 200)
+
+    def test_context(self):
+        self.client.login(username=self.username, password=self.password)
+        resp = self.client.get(self.base_url)
+        #context contains required fields
+        for key in self.context_keys:
+            self.assertTrue(key in resp.context)
+
+
+class AbstractViewPostTests(AbstractViewTests):
+    def test_post_login_required(self):
+        resp = self.client.post(self.base_url, follow=True)
+        self.assertEqual(len(resp.redirect_chain), 1) #exactly one redirect
+        #redirect is to login page
+        self.assertTrue(resp.redirect_chain[0][0].startswith("http://testserver/accounts/login/?next=" + self.base_url))
+        self.assertEqual(resp.redirect_chain[0][1], 302) #http code is 302
+        
+    
+
+class MainListTestCase(AbstractViewGetTests, TestCase):
+    base_url = "/"
+    context_keys = ['rooms', 'buildings']
+
+class AjaxMainListTextCase(AbstractViewGetTests, TestCase):
+    base_url = "/ajax_main_list/"
+    context_keys = ['items', 'locationinfo', 'pagetitle', 'content_url']
+
+
+class AjaxRoomViewTestCase(AbstractViewGetTests, TestCase):
+    base_url = "/ajax_room/"
+    context_keys = ['room', 'items', 'locationinfo', 'pagetitle', 'content_url']
+
+    def setUp(self):
+        super(AjaxRoomViewTestCase, self).setUp()
+        building = Building.objects.create(name="Alderwood", abbrev="ALD")
+        room = Room.objects.create(number="101", building=building)
+        self.base_url += room.id + "/"
+
+class AjaxEditItemTestCase(AbstractViewGetTests, TestCase):
+    base_url = "/ajax_edit_item/"
+    context_keys = ['item', 'title', 'submit_url']
+    
+    def setUp(self):
+        super(AjaxEditItemTestCase, self).setUp()
+        item = Item.objects.create(itemType="Computer", manufacturer="HP", model="DC8300")
+        self.base_url += item.id + "/"
+
+"""
+    def get_context_data(self, **kwargs):
+        context = super(AjaxEditItem, self).get_context_data(**kwargs)
+        context['item'] = get_object_or_404(Item, id=self.args[0])
+        context['title'] = "Edit Item Form"
+        context['submit_url'] = "/ajax_edit_item/" + context['item'].id + "/"
+        return context
+
+    def get_form(self, form_class=None):
+        item = get_object_or_404(Item, id=self.args[0])
+        return ItemForm(self.request.POST, instance=item)
+
+    def form_invalid(self, form):
+        return HttpResponse("Invalid Form Data", content_type="text/plain")
+
+    def form_valid(self, form):
+        form.save()
+        return HttpResponse("Saved", content_type="text/plain")
+"""
+class AjaxAddItemTestCase(AbstractViewGetTests, TestCase):
+    base_url = "/ajax_add_item/"
+    context_keys = ['title', 'submit_url']
+
+class AjaxAddItemToRoomTestCase(AbstractViewGetTests, AbstractViewPostTests, TestCase):
+    base_url = "/ajax_add_item/"
+    context_keys = ['room', 'title', 'submit_url']
+
+    def setUp(self):
+        super(AjaxAddItemToRoomTestCase, self).setUp()
+        building = Building.objects.create(name="Alderwood", abbrev="ALD")
+        room = Room.objects.create(number="101", building=building)
+        self.base_url += room.id + "/"
+
+"""
+class AjaxAddItem(AjaxEditItem):
+
+    def get_context_data(self, **kwargs):
+        context = super(AjaxEditItem, self).get_context_data(**kwargs) #grandparent's method
+        if(len(self.args) >= 1): #if room id has been passed
+            context['room'] = get_object_or_404(Room, id=self.args[0])
+        room_id = (context['room'].id + "/") if ('room' in context) else ("")
+        context['title'] = "Add Item Form"
+        context['submit_url'] = "/ajax_add_item/" + room_id
+        return context
+
+    def get_form(self, form_class=None):
+        return ItemForm(self.request.POST)
+"""
