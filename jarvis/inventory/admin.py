599fb88f1fecc4989078509cba18c4958f0df765
Jeff Graham <jeffsgraham@uw.edu>
2016-07-13 06:41:30 -0700

    Updated foreignkey fields to point at newly created unique indexes on the name field of Model, Manufacturer, and Type. Changed the admin site forms for these models to include a cascade update when their name fields are update in order to enforce data integrity.

--- jarvis/inventory/admin.py
+++ jarvis/inventory/admin.py
@@ -1,9 +1,31 @@
 from django.contrib import admin
+from django import forms
 from models import *
 
-#exclude list field from admin interface
+class CascadeNameChangeForm(forms.ModelForm):
+    #override save to update related objects when name changes
+    def save(self, *args, **kwargs):
+        #check that this is an update operation
+        if self.instance.pk is not None and 'name' in self.changed_data:
+            #get old version of object from database
+            old = type(self.instance).objects.filter(pk=self.instance.pk)[0]
+            #call instance's cascade_name_change() to update related objects
+            self.instance.cascade_name_change(old)
+        #save the pending changes and return
+        return super(CascadeNameChangeForm, self).save(*args, **kwargs)
+
+class ItemModelAdminForm(CascadeNameChangeForm):
+    class Meta:
+        model = Model
+        exclude = ['partNumbers']
+
 class ItemModelAdmin(admin.ModelAdmin):
+    form = ItemModelAdminForm
+    #exclude list field from admin interface
     exclude = ('partNumbers',)
+
+class CascadeNameChangeAdmin(admin.ModelAdmin):
+    form = CascadeNameChangeForm
 
 class ItemAdmin(admin.ModelAdmin):
     exclude = ('attributes','uptime',)
@@ -17,9 +39,9 @@ class ItemAdmin(admin.ModelAdmin):
         return item.attributes.get('IP Address', '')
     getIPAddress.short_description = 'IP Address'
 
-admin.site.register(Type)
+admin.site.register(Type, CascadeNameChangeAdmin)
 admin.site.register(Model, ItemModelAdmin)
-admin.site.register(Manufacturer)
+admin.site.register(Manufacturer, CascadeNameChangeAdmin)
 admin.site.register(Attribute)
 admin.site.register(IPRange)
 admin.site.register(Building)
